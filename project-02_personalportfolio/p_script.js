00ad3f6c228bed65381f57be505fdd4122ec8a53
sserrano-risd <sserrano@risd.edu>
2021-02-01 19:43:18 -0500

    commit.new

--- project-02_personalportfolio/p_script.js
+++ project-02_personalportfolio/p_script.js
@@ -1,9 +1,38 @@
 $("document").ready(function(){
 
-    $("img").mouseover(function () {
+    $("#ic01").mouseover(function () {
         $(this).css('opacity','.15');
+        $("#plani").show();
     });
-    $("img").mouseout(function () {
+    $("#ic01").mouseout(function () {
         $(this).css('opacity','1');
+        $("#plani").hide();
     });
+
+    $("#ic02").mouseover(function () {
+         $(this).css('opacity','.15');
+         $("#tri").show();
+    });
+    $("#ic02").mouseout(function () {
+         $(this).css('opacity','1');
+         $("#tri").hide();
+    });
+
+    $("#ic03").mouseover(function () {
+         $(this).css('opacity','.15');
+         $("#geoid").show();
+    });
+    $("#ic03").mouseout(function () {
+         $(this).css('opacity','1');
+         $("#geoid").hide();
+    });
+    
+    $("#ic04").mouseover(function () {
+        $(this).css('opacity','.15');
+        $("#browns").show();
+   });
+   $("#ic04").mouseout(function () {
+        $(this).css('opacity','1');
+        $("#browns").hide();
+   });
 })
